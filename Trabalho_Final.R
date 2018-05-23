e0bea588f1e435ad05a64ae38bcbc5158e7e7459
thiagom <thiagom@motorola.com>
2018-05-23 14:42:46 -0300

    Added changes

--- Trabalho_Final.R
+++ Trabalho_Final.R
@@ -4,6 +4,9 @@
 #        Thiago Gomes Marçal Pereira
 ########################################################################
 
+# Libraries
+library(cluster)
+
 # Set Working Directory
 setwd("/Users/thiagom/Documents/Studies/Unicamp/MDC/INF-613/Tarefas/Final/INF0613_Trabalho_Final")
 
@@ -15,18 +18,28 @@ features <- read.csv("dataset/features.csv", sep=",", header = T)
 headlines$publish_date <- strptime(headlines$publish_date, "%Y%m%d")
 
 # Run PCA for features
-features.pca <- prcomp(features, scale. = T)
+#features.pca <- prcomp(features, scale. = T)
 
 # Change maxPrint, to be able to print PCA
-# options(max.print=25000)
+options(max.print=25000)
 
 # Print PCA to check for Cumulative Proportions
 summary(features.pca)
 # 85% - PC1654
 # 90% - PC1804
 
-# To the following Items use PC1700 - Cumulative Proportion - 0.86584
-PC <- 1700
-dados <- features.pca$x[1:PC]
+# To the following Items use PC1654 - ~85%
+PC <- 1654
+dados <- features.pca$x[,1:PC]
 
-# Run k-means and k-medoids for the data
+# Run k-means and k-medoids for the data (5, 10, 15, 20 clusters)
+set.seed(123)
+rows <- nrow(dados)
+amostra <- dados[sample(rows, rows*0.25),]
+#features.kmeans <- kmeans(dados, 5)
+amostra.kmeans <- kmeans(amostra, 5)
+
+# Calculatte Silhouette Coeficient 
+#features.silhouette <- silhouette(features.kmeans$cluster, dados)
+#summary(features.silhouette)$avg.width
+amostra.silhouette <- silhouette(amostra.kmeans$cluster, amostra)
