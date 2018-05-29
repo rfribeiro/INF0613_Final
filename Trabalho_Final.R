6a71d3644feb85462ffb7f1b4a57ca198f3eb446
thiagom <thiagom@motorola.com>
2018-05-28 23:03:09 -0300

    Calculando os Kmeans e Coeficientes de silhueta

--- Trabalho_Final.R
+++ Trabalho_Final.R
@@ -37,15 +37,47 @@ dados <- features.pca$x[,1:PC]
 d <- dist(dados)
 
 # Run k-means and k-medoids for the data (5, 10, 15, 20 clusters)
-set.seed(123)
-features.kmeans <- kmeans(dados, 5)
+set.seed(1234)
+features.kmeansk5 <- kmeans(dados, 5)
+set.seed(1234)
+features.kmeansk10 <- kmeans(dados, 10)
+set.seed(1234)
+features.kmeansk15 <- kmeans(dados, 15)
+set.seed(1234)
+features.kmeansk20 <- kmeans(dados, 20)
 
 # Calculatte Silhouette Coeficient 
-features.silhouette <- silhouette(features.kmeans$cluster, d)
+features.silhouettek5 <- silhouette(features.kmeansk5$cluster, d)
+features.silhouettek10 <- silhouette(features.kmeansk10$cluster, d)
+features.silhouettek15 <- silhouette(features.kmeansk15$cluster, d)
+features.silhouettek20 <- silhouette(features.kmeansk20$cluster, d)
+
+# Erros Quadraticos
+print("Erros Quadráticos")
+print("5")
+features.kmeansk5$tot.withinss
+print("10")
+features.kmeansk10$tot.withinss
+print("15")
+features.kmeansk15$tot.withinss
+print("20")
+features.kmeansk20$tot.withinss
+
+# Coeficientes de Silhueta
+print("Coeficientes de Silhueta")
+print("5")
+summary(features.silhouettek5)$avg.width
+print("10")
+summary(features.silhouettek10)$avg.width
+print("15")
+summary(features.silhouettek15)$avg.width
+print("20")
+summary(features.silhouettek20)$avg.width
 
 # Calculate the bi-grams using NLP's ngrams method
-bigramas <- list()
-for (cl in c(1:5)) {
-  words <- colnames(features)[features.kmeans$cluster == cl]
-  bigramas[[cl]] <- ngrams(words, 2)
-}
+#bigramas <- list()
+#for (cl in c(1:5)) {
+#  words <- colnames(features)[features.kmeans$cluster == cl]
+#  bigramas[[cl]] <- ngrams(words, 2)
+#}
+
